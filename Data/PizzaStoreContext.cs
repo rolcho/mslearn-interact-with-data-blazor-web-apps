82adbda25397666aa76ae3e49d111f3bc919e0c3
rolcho <roland@cimem.hu>
2024-09-13 15:25:36 +0200

    create api and db

---
+++ Data/PizzaStoreContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazingPizza.Data;
+
+public class PizzaStoreContext : DbContext
+{
+    public PizzaStoreContext(DbContextOptions options)
+        : base(options) { }
+
+    public DbSet<PizzaSpecial> Specials { get; set; }
+}
